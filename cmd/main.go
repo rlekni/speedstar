c4e60dbe627fa151ce4730ae98e6127e70ab1898
rlekni <3824689+rlekni@users.noreply.github.com>
2024-02-27 23:23:05 +0000

    testing scheduler

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,65 @@ package main
 
 import (
 	"fmt"
+	"time"
 
+	"github.com/go-co-op/gocron/v2"
 	"github.com/showwin/speedtest-go/speedtest"
 )
 
 func main() {
+	go runScheduler()
+
+	for {
+		time.Sleep(1000)
+	}
+}
+
+func runScheduler() {
+	// create a scheduler
+	s, err := gocron.NewScheduler()
+	// defer func() { _ = s.Shutdown() }()
+	// defer s.Shutdown()
+	if err != nil {
+		// handle error
+	}
+
+	// add a job to the scheduler
+	j, err := s.NewJob(
+		gocron.DurationJob(
+			10*time.Second,
+		),
+		gocron.NewTask(
+			func(a string, b int) {
+				// do things
+				fmt.Println("JOB RUN")
+			},
+			"hello",
+			1,
+		),
+	)
+	if err != nil {
+		// handle error
+	}
+	// each job has a unique id
+	fmt.Println(j.ID())
+
+	// start the scheduler
+	s.Start()
+
+	// block until you are ready to shut down
+	// select {
+	// case <-time.After(time.Minute):
+	// }
+
+	// // when you're done, shut it down
+	// err = s.Shutdown()
+	// if err != nil {
+	// 	// handle error
+	// }
+}
+
+func testSpeed() {
 	fmt.Println("Hi")
 	var speedtestClient = speedtest.New()
 	// Get user's network information
