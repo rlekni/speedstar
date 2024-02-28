76741343da00375fa5910044a4f205551d24fab7
rlekni <3824689+rlekni@users.noreply.github.com>
2024-02-28 15:09:58 +0000

    update speed test service

--- internal/services/speedservice.go
+++ internal/services/speedservice.go
@@ -2,9 +2,44 @@ package services
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/showwin/speedtest-go/speedtest"
 )
+
+type ISpeedTestService interface {
+	RunSpeedtest()
+}
+
+type SpeedTestService struct {
+	client *speedtest.Speedtest
+}
+
+func NewSpeedtestService() ISpeedTestService {
+	var speedtestClient = speedtest.New()
+	return &SpeedTestService{
+		client: speedtestClient,
+	}
+}
+
+func (service SpeedTestService) RunSpeedtest() {
+	user, _ := service.client.FetchUserInfo()
+	log.Printf("ISP %s\n", user.Isp)
+
+	serverList, _ := service.client.FetchServers()
+	targets, _ := serverList.FindServer([]int{})
+	for _, server := range targets {
+		log.Printf("Server: %s", server.Name)
+		// Please make sure your host can access this test server,
+		// otherwise you will get an error.
+		// It is recommended to replace a server at this time
+		server.PingTest(nil)
+		server.DownloadTest()
+		server.UploadTest()
+		log.Printf("Server: %s; Latency: %s, Download: %f, Upload: %f\n", server.Name, server.Latency, server.DLSpeed, server.ULSpeed)
+		server.Context.Reset() // reset counter
+	}
+}
 
 func TestSpeed() {
 	fmt.Println("Hi")
