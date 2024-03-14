31da1a60630530ad9ae81c56755587c2b609f9eb
rlekni <3824689+rlekni@users.noreply.github.com>
2024-03-14 13:57:45 +0000

    Add size and duration

--- internal/services/speedservice.go
+++ internal/services/speedservice.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"speedstar/internal/db"
 	"speedstar/internal/types"
+	"time"
 
 	"github.com/showwin/speedtest-go/speedtest"
 )
@@ -46,22 +47,36 @@ func (service SpeedtestService) RunSpeedtest() {
 		// otherwise you will get an error.
 		// It is recommended to replace a server at this time
 		server.PingTest(nil)
+		startDownload := time.Now()
 		server.DownloadTest()
+		downloadDuration := time.Since(startDownload)
+
+		startUpload := time.Now()
 		server.UploadTest()
+		uploadDuration := time.Since(startUpload)
+
 		log.Printf("Server: %s; Latency: %s, Download: %f, Upload: %f\n", server.Name, server.Latency, server.DLSpeed, server.ULSpeed)
 		log.Printf("Jitter: %s\n", server.Jitter)
 		log.Printf("Server ID: %s", server.ID)
 
+		downloadSize := downloadDuration.Seconds() * server.DLSpeed
+		uploadSize := uploadDuration.Seconds() * server.ULSpeed
+		log.Printf("Download Time: %f, Download Size: %f", downloadDuration.Seconds(), downloadSize)
+		log.Printf("Upload Time: %f, Upload Size: %f", uploadDuration.Seconds(), uploadSize)
 		result := types.SpeedtestResult{
-			Isp:       user.Isp,
-			Server:    server.Name,
-			Latitude:  server.Lat,
-			Longitude: server.Lon,
-			Distance:  server.Distance,
-			Latency:   server.Latency.Milliseconds(),
-			Jitter:    server.Jitter.Microseconds(),
-			Download:  server.DLSpeed,
-			Upload:    server.ULSpeed,
+			Isp:              user.Isp,
+			Server:           server.Name,
+			Latitude:         server.Lat,
+			Longitude:        server.Lon,
+			Distance:         server.Distance,
+			Latency:          server.Latency.Milliseconds(),
+			Jitter:           server.Jitter.Microseconds(),
+			Download:         server.DLSpeed,
+			Upload:           server.ULSpeed,
+			DownloadDuration: downloadDuration.Seconds(),
+			DownloadSize:     downloadSize,
+			UploadDuration:   uploadDuration.Seconds(),
+			UploadSize:       uploadSize,
 		}
 		service.repository.SaveSpeedtestResults(result)
 		server.Context.Reset() // reset counter
--- internal/types/types.go
+++ internal/types/types.go
@@ -1,13 +1,17 @@
 package types
 
 type SpeedtestResult struct {
-	Isp       string
-	Server    string
-	Latitude  string
-	Longitude string
-	Distance  float64
-	Latency   int64
-	Jitter    int64
-	Download  float64
-	Upload    float64
+	Isp              string
+	Server           string
+	Latitude         string
+	Longitude        string
+	Distance         float64
+	Latency          int64
+	Jitter           int64
+	Download         float64
+	Upload           float64
+	DownloadDuration float64
+	UploadDuration   float64
+	DownloadSize     float64
+	UploadSize       float64
 }
